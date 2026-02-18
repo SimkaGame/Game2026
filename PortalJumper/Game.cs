a8103278bf059a1b64b49072baf676f4fd62649c
simka.minin@yandex.ru <131748949+SimkaGame@users.noreply.github.com>
2026-02-18 13:24:52 +0300

    Create files game and programm contain deafault methods: update,render,handleinput

---
+++ PortalJumper/Game.cs
@@ -0,0 +1,43 @@
+using System;
+
+namespace PortalJumper
+{
+    public class Game
+    {
+        private bool isRunning = true;
+
+        public void Run()
+        {
+            while (isRunning)
+            {
+                HandleInput();
+                Update();
+                Render();
+                Thread.Sleep(40);
+            }
+        }
+
+        private void HandleInput()
+        {
+            if (!Console.KeyAvailable) return;
+            var key = Console.ReadKey(true);
+
+            if (key.Key == ConsoleKey.Escape)
+            {
+                isRunning = false;
+            }
+        }
+
+        private void Update()
+        {
+            
+        }
+
+        private void Render()
+        {
+            Console.Clear();
+            Console.WriteLine("Portal Jumper");
+            Console.WriteLine("Esc - Выход");
+        }
+    }
+}
---
+++ PortalJumper/Program.cs
@@ -0,0 +1,10 @@
+namespace PortalJumper;
+
+class Program
+{
+    static void Main()
+    {
+        var game = new Game();
+        game.Run();
+    }
+}
