71ab29a3978c2501d321c31db3bbdd5530916946
simka.minin@yandex.ru <131748949+SimkaGame@users.noreply.github.com>
2026-04-25 15:10:55 +0300

    UI и HUD

--- PortalJumper/Entities/SpeedDecorator.cs
+++ PortalJumper/Entities/SpeedDecorator.cs
@@ -1,12 +1,31 @@
-namespace PortalJumper.Entities;
+using System;
 using PortalJumper.Core.Interfaces;
 
-public class WindDecorator : IAttackable
+namespace PortalJumper.Entities;
+
+public class SpeedDecorator : IAttackable
 {
     private readonly IAttackable _inner;
-    public int Hp { get => _inner.Hp; set => _inner.Hp = value; }
 
-    public WindDecorator(IAttackable entity) => _inner = entity;
+    public int Hp 
+    { 
+        get => _inner.Hp; 
+        set => _inner.Hp = value; 
+    }
+
+    public int MaxHp 
+    { 
+        get => _inner.MaxHp; 
+        set => _inner.MaxHp = value; 
+    }
+
+    public event Action<int, int> OnHealthChanged
+    {
+        add => _inner.OnHealthChanged += value;
+        remove => _inner.OnHealthChanged -= value;
+    }
+
+    public SpeedDecorator(IAttackable entity) => _inner = entity;
 
     public void TakeDamage(int damage) => _inner.TakeDamage(damage);
 
